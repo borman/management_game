f841fd6fa06a7cdc576a4577096165a2b8a0e729
Mikhail Borisov <borisov.mikhail@gmail.com>
2010-12-07 10:32:20 +0300

    Removed verbose FSM debugging output

--- src/core/fsm.c
+++ src/core/fsm.c
@@ -4,7 +4,6 @@
 
 void fsm_init(FSM *fsm, int init_state)
 {
-  trace("FSM %s init(%s)", fsm->name, fsm->states[init_state].name);
   fsm->state = init_state;
   fsm->next_state = init_state;
   fsm->loop_finished = 0;
@@ -15,25 +14,12 @@ void fsm_init(FSM *fsm, int init_state)
 
 void fsm_event(FSM *fsm, FSMEvent *event)
 {
-  trace("FSM %s(%s) event %d from %d", 
-      fsm->name, fsm->states[fsm->state].name,
-      event->type, event->fd);
-
   fsm->states[fsm->state].on_event(fsm, event);
   while (fsm->loop_finished)
   {
-    trace("FSM %s(%s) loop finished -> switch state", 
-        fsm->name, fsm->states[fsm->state].name);
-
     fsm->states[fsm->state].on_exit(fsm);
-    
-    trace("FSM %s: %s -> %s", fsm->name, 
-        fsm->states[fsm->state].name, 
-        fsm->states[fsm->next_state].name);
-
     fsm->state = fsm->next_state;
     fsm->loop_finished = 0;
-
     fsm->states[fsm->state].on_enter(fsm);
   }
 }
@@ -41,8 +27,6 @@ void fsm_event(FSM *fsm, FSMEvent *event)
 
 void fsm_finish_loop(FSM *fsm)
 {
-  trace("FSM %s(%s) req finish loop", 
-      fsm->name, fsm->states[fsm->state].name);
   fsm->loop_finished = 1;
 }
 

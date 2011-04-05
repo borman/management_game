d63996fd4766855b0ed6316dd186adb039aaf0f6
Mikhail Borisov <borisov.mikhail@gmail.com>
2011-04-06 00:12:38 +0400

    First steps towards scripting

--- src/client/BotActor.h
+++ src/client/BotActor.h
@@ -2,14 +2,31 @@
 #define BOTACTOR_H
 
 #include "Actor.h"
+#include "LoadedProgram.h"
+#include "Executor.h"
+#include "Builtin.h"
+#include "BasicBuiltin.h"
+#include "Context.h"
 
-class BotActor: public Actor
+class BotActor: public Actor, public ListedBuiltin
 {
   public:
+    BotActor(LoadedProgram &program);
+
     virtual void onGameStart(Session *session);
     virtual void onTurn(Session *session);
   private:
-    unsigned int roundCounter;
+    // [rawCount, rawPrice, productCount, productPrice]
+    static void marketState(ListedBuiltin *self, Context &context);
+    
+    static void buy(ListedBuiltin *self, Context &context);
+    static void sell(ListedBuiltin *self, Context &context);
+    static void produce(ListedBuiltin *self, Context &context);
+
+    static const ListedBuiltin::Definition defs[];
+    Executor m_executor;
+    BasicBuiltin m_basis;
+    Session *m_thisSession;
 };
 
 #endif // BOTACTOR_H
---
+++ src/client/DumbActor.cpp
@@ -0,0 +1,47 @@
+#include <cassert>
+#include <cstdio>
+#include "Exceptions.h"
+#include "Stanza.h"
+#include "DumbActor.h"
+#include "Session.h"
+#include "StdLib.h"
+#include "Term.h"
+
+void DumbActor::onGameStart(Session *session)
+{
+  assert(session != NULL);
+  
+  roundCounter = 0;
+}
+
+void DumbActor::onTurn(Session *session)
+{
+  assert(session != NULL);
+
+  roundCounter++;
+  printf("\n");
+  printf("%s%s<=----------%s      Round %-5u %s---------=>%s%s\n",
+      Term::SetBold, Term::SetBrown, Term::ResetColor,
+      roundCounter,
+      Term::SetBrown, Term::ResetColor, Term::SetRegular);
+
+  session->gameInfo().printTransactions(stdout);
+  session->gameInfo().printPlayers(stdout);
+  session->gameInfo().printMarket(stdout);
+
+  const MarketState &market = session->gameInfo().market();
+
+  unsigned int rawToBuy = min(market.rawCount(), 2U); 
+  unsigned int rawPrice = market.rawPrice();
+
+  unsigned int productToSell = min(market.productCount(), 2U);
+  unsigned int productPrice = market.productPrice();
+
+  unsigned int productToProduce = 2;
+
+  session->buy(rawToBuy, rawPrice);
+  session->sell(productToSell, productPrice);
+  session->produce(productToProduce);
+}
+
+
